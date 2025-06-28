84a47479f57cfdd4471093bb97fd53134f5a5889
SujanHumagain26 <sujanhumagain26@gmail.com>
2025-06-28 21:12:13 +0545

    Updated

--- app/head.tsx
+++ app/head.tsx
@@ -2,23 +2,23 @@ export default function Head() {
   return (
     <>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="I'm Aashutosh Chaudhary, Network and Security Engineer based in Kathmandu, Nepal" />
-      <meta name="keywords" content="aashutosh chaudhary, portfolio, aashutosh, cybersecurity, information security, security engineer, portfolio website, personal portfolio" />
-      <meta property="og:site_name" content="Aashutosh Chaudhary's Portfolio" />
-      <meta property="og:title" content="Aashutosh Chaudhary" />
+      <meta name="description" content="I'm Sujan Humagain, System and Security Engineer based in Kathmandu, Nepal" />
+      <meta name="keywords" content="sujan humagain, portfolio, sujan, cybersecurity, information security, security engineer, portfolio website, personal portfolio" />
+      <meta property="og:site_name" content="Sujan Humagain's Portfolio" />
+      <meta property="og:title" content="Sujan Humagain" />
       <meta property="og:description"
-        content="I'm Aashutosh Chaudhary, Network and Security Engineer based in Kathmandu, Nepal" />
-      <meta property="og:url" content="https://aashutoshchaudhary-obe66rer2-aashutosh-chys-projects.vercel.app" />
+        content="I'm Sujan Humagain, System and Security Engineer based in Kathmandu, Nepal" />
+      <meta property="og:url" content="portfolio-sujan-15z65p4zh-sujans-projects-467cc741.vercel.app" />
       <meta property="og:image" content="https://res.cloudinary.com/dqzeyhnuy/image/upload/v1749367873/next-gen_ealtah.png" />
-      <link rel="canonical" href="https://aashutoshchaudhary-obe66rer2-aashutosh-chys-projects.vercel.app" />
-      <meta name="author" content="Aashutosh Chaudhary" />
+      <link rel="canonical" href="portfolio-sujan-15z65p4zh-sujans-projects-467cc741.vercel.app" />
+      <meta name="author" content="Sujan Humagain" />
       <meta name="language" content="English" />
       <meta name="twitter:card" content="summary" />
-      <meta name="twitter:site" content="https://aashutoshchaudhary-obe66rer2-aashutosh-chys-projects.vercel.app" />
-      <meta name="application-name" content="Aashutosh Chaudhary" />
-      <meta name="apple-mobile-web-app-title" content="Aashutosh Chaudhary" />
+      <meta name="twitter:site" content="portfolio-sujan-15z65p4zh-sujans-projects-467cc741.vercel.app" />
+      <meta name="application-name" content="Sujan Humagain" />
+      <meta name="apple-mobile-web-app-title" content="Sujan Humagain" />
       <link rel="icon" href="/favicon.ico" />
-      <title>Aashutosh Chaudhary</title>
+      <title>Sujan Humagain</title>
     </>
   )
 }
